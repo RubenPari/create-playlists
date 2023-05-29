3baf2e37779045614307689190bf5f41fc75debc
RubenPari <rubenpari.developer@gmail.com>
2023-05-30 00:52:05 +0200

    to complete deleteAllTracksFromPlaylist

---
+++ src/lib/spotify/playlistLib.ts
@@ -0,0 +1,73 @@
+import SpotifyWebApi from 'spotify-web-api-node';
+
+export class PlaylistLib {
+  private client: SpotifyWebApi;
+
+  constructor(client: SpotifyWebApi) {
+    this.client = client;
+  }
+
+  /**
+   * Get all tracks from a playlist
+   */
+  async getPlaylistTracks(playlistId: string) {
+    const tracks: SpotifyApi.PlaylistTrackObject[] = [];
+    let snapshotId: string;
+
+    const limit = 50;
+    let offset = 0;
+
+    while (true) {
+      const playlistTracks = await this.client.getPlaylistTracks(playlistId, {
+        limit,
+        offset,
+      });
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      snapshotId = playlistTracks.body.snapshot_id;
+
+      tracks.push(...playlistTracks.body.items);
+
+      if (playlistTracks.body.next) {
+        offset += limit;
+      } else {
+        break;
+      }
+    }
+
+    return {
+      tracks: tracks,
+      snapshotId: snapshotId,
+    };
+  }
+
+  /**
+   * Delete all tracks from a playlist
+   */
+  async deleteAllTracksFromPlaylist(playlistId: string) {
+    const { tracks, snapshotId } = await this.getPlaylistTracks(
+      playlistId,
+    );
+
+    const uris = tracks.map((track) => track.track.uri);
+
+    const limit = 100;
+    let offset = 0;
+
+    while (true) {
+      await this.client.removeTracksFromPlaylist(
+        playlistId,
+        uris.slice(offset, offset + limit),
+      );
+
+      if (offset + limit < uris.length) {
+        offset += limit;
+      } else {
+        break;
+      }
+    }
+
+    return true;
+  }
+}
